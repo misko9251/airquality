6c0d3cc1de03e628166b7bd1af1b27be94a1d378
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-07-26 21:55:03 -0400

    added h3 to data ejs

--- public/js/main.js
+++ public/js/main.js
@@ -24,7 +24,7 @@ let lng = Object.values(params)[1]
 
 
 async function fetchAQI(){
-    const response = await fetch(`https://api.waqi.info/feed/geo:${lat};${lng}/?token=`);
+    const response = await fetch(`https://api.waqi.info/feed/geo:${lat};${lng}/?token=3ae275f437e5587b7eabbc738d8f07aab52665a8`);
     const aqiData = await response.json();
     let aqi = (aqiData.data.aqi);
     document.getElementById('aqi').innerText = aqi
